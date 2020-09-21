fb48b1e9a798ddbbd102ed37f351bcf091215064
Owen <owenmllm@gmail.com>
2020-09-21 09:48:19 -0500

    Update starfield.py

--- starfield.py
+++ starfield.py
@@ -1,3 +1,4 @@
+#cerner_2^5_2020
 for _ in range(int(__import__("sys").argv[2])): print("".join([(__import__("random").choice([".", "*", "+", "o"] + [" " for _ in range(int(__import__("sys").argv[1]))])) for _ in range(int(__import__("sys").argv[2]))]))
 
 '''

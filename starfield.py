eab5d996e1e5173a8fa823c7986c828404dcf5b5
Owen <owenmllm@gmail.com>
2020-09-21 09:47:45 -0500

    Add files via upload

---
+++ starfield.py
@@ -0,0 +1,27 @@
+for _ in range(int(__import__("sys").argv[2])): print("".join([(__import__("random").choice([".", "*", "+", "o"] + [" " for _ in range(int(__import__("sys").argv[1]))])) for _ in range(int(__import__("sys").argv[2]))]))
+
+'''
+python starfield.py <emptiness> <size>
+Set emptiness to 5 for Hubble deep field, 50 for night sky
+PS C:\dev\personal> python .\starfield.py 5 20
+o+.    +*o * * o  o*
+  *  +  *  +   *+ o 
+ o * .+   o+*  ..
+ .    .  *o*    +*
++ +*     o   .   *+o
+  +     o*   * +    
+ o  * o**o    o .  .
+  *  . .     ..  o.
+o +*+o*.  *    ++ .
+**o+ .  o *      +.
+   o.+  .o....o   *
+  +*  .o  .   *  *
+ * ** ** .o .o   .
+    *   *     +  . .
+. +  *  o* .     ooo
+   . .+*  o.+  * .o+
++ +.  * .      .   +
+ o+.   o  +*. o ..*
+      *  ++o     . *
+  * *   .* *o * +. o
+'''
